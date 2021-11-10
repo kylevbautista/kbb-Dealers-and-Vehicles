155cfb2344479c22dc942d72783e7da079a63ccf
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-09 19:18:49 -0700

    update api calls add helper api functions

--- src/api/coxApi.js
+++ src/api/coxApi.js
@@ -16,6 +16,15 @@ export const getDealerInfo = async (datasetId, dealerid) => {
   return data;
 };
 
+// async function to concurrently fetch dealer info from a list of dealers ids
+export const getAllDealerInfo = async (datasetId, dealerids) => {
+  const response = await Promise.all(
+    dealerids.map((id) => fetch(`${baseUrl}/api/${datasetId}/dealers/${id}`))
+  );
+  const data = await Promise.all(response.map((res) => res.json()));
+  return data;
+};
+
 // async function to fetch vehicle ids
 export const getVehiclesIds = async (datasetId) => {
   const response = await fetch(`${baseUrl}/api/${datasetId}/vehicles`);
@@ -23,11 +32,49 @@ export const getVehiclesIds = async (datasetId) => {
   return data;
 };
 
-// async function to fetch
+// async function to fetch vehicleinfo
 export const getVehicleInfo = async (datasetId, vehicleid) => {
   const response = await fetch(
     `${baseUrl}/api/${datasetId}/vehicles/${vehicleid}`
   );
   const data = await response.json();
+  return data;
+};
+
+// async fucntion to concurrently fetch vehicle info from a list of vehicle ids
+export const getAllVehicleInfo = async (datasetId, vehicleids) => {
+  const response = await Promise.all(
+    vehicleids.map((id) => fetch(`${baseUrl}/api/${datasetId}/vehicles/${id}`))
+  );
+  const data = await Promise.all(response.map((res) => res.json()));
+  return data;
+};
+
+export const addVehicles = (dealerId, vehicleInfo) => {
+  const temp = [];
+  for (let i = 0; i < vehicleInfo.length; i++) {
+    if (dealerId === vehicleInfo[i].dealerId) {
+      const newVehicleObj = Object.assign({}, vehicleInfo[i]);
+      delete newVehicleObj.dealerId;
+      temp.push(newVehicleObj);
+    }
+  }
+  return temp;
+};
+
+// async function to submit ans and then return response
+export const submitDataSet = async (datasetId, ans) => {
+  const baseUrl = "http://api.coxauto-interview.com";
+  const response = await fetch(`${baseUrl}/api/${datasetId}/answer`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ans),
+  });
+
+  const data = await response.json();
+
   return data;
 };
---
+++ src/components/ManageHomePage.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import * as coxApi from "../api/coxApi";
+
+function HomePage() {
+  const [dataSet, setDataSet] = useState({});
+  const [vehicleIds, setVehicleIds] = useState([]);
+  // const [vehicleInfo, setVehicleInfo] = useState([]);
+  const [allVehicleInfo, setAllVehicleInfo] = useState([]);
+  const [allDealerInfo, setAllDealerInfo] = useState({});
+
+  const getDataSetInfo = async () => {
+    const ans = { dealers: [] };
+    const dataset = await coxApi.getDataSet();
+    setDataSet(dataset);
+
+    const vehiclesids = await coxApi.getVehiclesIds(dataset.datasetId);
+    setVehicleIds(vehiclesids.vehicleIds);
+    console.log(vehiclesids);
+
+    // const vehicleinfo = await coxApi.getVehicleInfo(
+    //   dataset.datasetId,
+    //   vehiclesids.vehicleIds[0]
+    // );
+    // setVehicleInfo(vehicleinfo);
+    // console.log(vehicleinfo);
+    const allvehicleinfo = await coxApi.getAllVehicleInfo(
+      dataset.datasetId,
+      vehiclesids.vehicleIds
+    );
+    setAllVehicleInfo(allvehicleinfo);
+    console.log("all vehicle", allvehicleinfo);
+
+    // for loop to add unique dealerids to array to use for getAllDealerInfo
+    const dealerIds = [];
+    for (let i = 0; i < allvehicleinfo.length; i++) {
+      if (dealerIds.indexOf(allvehicleinfo[i].dealerId) === -1) {
+        dealerIds.push(allvehicleinfo[i].dealerId);
+      }
+    }
+    const alldealerinfo = await coxApi.getAllDealerInfo(
+      dataset.datasetId,
+      dealerIds
+    );
+    console.log("all dealers", alldealerinfo);
+
+    for (let i = 0; i < alldealerinfo.length; i++) {
+      if (ans.dealers.indexOf(alldealerinfo[i].dealerId) === -1) {
+        const vehicleArray = coxApi.addVehicles(
+          alldealerinfo[i].dealerId,
+          allvehicleinfo
+        );
+        ans.dealers.push({ ...alldealerinfo[i], vehicles: vehicleArray });
+      }
+    }
+    console.log(ans);
+    const submitDataSetResponse = await coxApi.submitDataSet(
+      dataset.datasetId,
+      ans
+    );
+    console.log(submitDataSetResponse);
+  };
+
+  useEffect(() => {
+    getDataSetInfo();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        {Object.keys(dataSet).length > 0 ? (
+          <div className="col-3">datasetId: {dataSet.datasetId}</div>
+        ) : (
+          <div className="col-3">Loading...</div>
+        )}
+        <div className="col-3">
+          {vehicleIds.length > 0 ? (
+            vehicleIds.map((id) => <p key={id}>vehicleId:{id}</p>)
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+        <div className="col-3">
+          <div
+            className="container border"
+            style={{
+              height: "500px",
+              width: "100%",
+              overflowY: "scroll",
+            }}
+          >
+            {allVehicleInfo.length > 0 ? (
+              allVehicleInfo.map((vehicle) => (
+                <div key={vehicle.vehicleId}>
+                  <p>{vehicle.vehicleId}</p>
+                  <p>{vehicle.year}</p>
+                  <p>{vehicle.make}</p>
+                  <p>{vehicle.model}</p>
+                  <hr />
+                </div>
+              ))
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
